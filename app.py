093b308e9f0efee97a1a3bbe5495dfb322904029
Orel ABK6 <orel.abecassis@ortmontreuil.fr>
2024-07-03 11:24:17 +0200

    update

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for, flash
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 import os
 import dotenv
 
@@ -22,6 +25,12 @@ class Person(db.Model):
     email = db.Column(db.String(120), nullable=False)
 
 
+class AddUserForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Create')
+
+
 # Create all database tables
 with app.app_context():
     db.create_all()
@@ -32,6 +41,17 @@ def hello_world():
     # Retrieve the list of "Person" from the mariadb database
     persons = Person.query.all()
     return render_template('index.html', persons=persons)
+
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    form = AddUserForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        flash(f'Utilisateur {name} avec l\'email {email} a été ajouté avec succès!', 'success')
+        return redirect(url_for('add_user'))
+    return render_template('createForm.html', form=form)
 
 
 if __name__ == '__main__':
